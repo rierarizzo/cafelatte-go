92199d7d43d86d855741d6e5bfe4f84661b35084
kenethriera <kenethrr@outlook.com>
2023-07-17 22:28:18 -0500

    Agrega pruebas unitarias a utils para "hashear" texto

--- internal/utils/security_test.go
+++ internal/utils/security_test.go
@@ -1,1 +1,43 @@
 package utils_test
+
+import (
+	"github.com/rierarizzo/cafelatte/internal/utils"
+	"golang.org/x/crypto/bcrypt"
+	"testing"
+)
+
+func TestHashText(t *testing.T) {
+	text := "alone again, naturally"
+	hash, err := utils.HashText(text)
+	if err != nil {
+		t.Errorf("Error hashing text: %v", err)
+	}
+
+	if len(hash) == 0 {
+		t.Error("Empty hash generated")
+	}
+
+	// Verificar que el hash generado sea válido
+	err = bcrypt.CompareHashAndPassword([]byte(hash), []byte(text))
+	if err != nil {
+		t.Errorf("Invalid hash generated: %v", err)
+	}
+}
+
+func TestCheckTextHash(t *testing.T) {
+	text := "alone again, naturally"
+	hash, _ := utils.HashText(text)
+
+	// Verificar que CheckTextHash retorne verdadero para una coincidencia de hash y texto
+	result := utils.CheckTextHash(hash, text)
+	if result != true {
+		t.Error("CheckTextHash returned false for matching hash and text")
+	}
+
+	// Verificar que CheckTextHash retorne falso para una no coincidencia de hash y texto
+	incorrectText := "wrongpassword"
+	result = utils.CheckTextHash(hash, incorrectText)
+	if result != false {
+		t.Error("CheckTextHash returned true for non-matching hash and text")
+	}
+}
