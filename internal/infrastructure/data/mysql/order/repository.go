69d6a43bf221e1130144005edb514aad18b4b34f
kenethriera <kenethrr@outlook.com>
2023-08-23 16:14:50 -0500

    Divide método del repositorio en funciones pequeñas

--- internal/infrastructure/data/mysql/order/repository.go
+++ internal/infrastructure/data/mysql/order/repository.go
@@ -15,27 +15,14 @@ type Repository struct {
 }
 
 func (r *Repository) InsertPurchaseOrder(order domain.Order) (int, *domain.AppError) {
-	rollbackAndError := func(tx *sqlx.Tx, err error) *domain.AppError {
-		_ = tx.Rollback()
-		logrus.WithField(misc.RequestIDKey, request.ID()).Error(err)
-
-		return domain.NewAppError(err, domain.RepositoryError)
-	}
-
 	tx, _ := r.db.Beginx()
 
-	model := fromOrderToModel(order)
-	query := `insert into PurchaseOrder (UserID, ShippingAddressID, PaymentMethodID, 
-        Notes, OrderedAt) values (?,?,?,?,?)`
-
-	res, err := tx.Exec(query, model.UserID, model.ShippingAddressID,
-		model.PaymentMethodID, model.Notes.String, time.Now())
-	if err != nil {
-		return 0, rollbackAndError(tx, err)
+	orderID, appErr := generatePurchaseOrderID(tx, order)
+	if appErr != nil {
+		return 0, appErr
 	}
-	orderID, _ := res.LastInsertId()
 
-	query = "insert into ProductInOrder (OrderID, ProductID, Quantity) values (?,?,?)"
+	query := "insert into ProductInOrder (OrderID, ProductID, Quantity) values (?,?,?)"
 
 	insertProductStmnt, err := tx.Prepare(query)
 	if err != nil {
@@ -72,36 +59,58 @@ func (r *Repository) InsertPurchaseOrder(order domain.Order) (int, *domain.AppEr
 		return 0, rollbackAndError(tx, err)
 	}
 
-	err = updateOrderAmount(tx, int(orderID))
+	appErr = updateOrderAmount(tx, orderID)
+	if appErr != nil {
+		return 0, appErr
+	}
+
+	return orderID, nil
+}
+
+func generatePurchaseOrderID(tx *sqlx.Tx, order domain.Order) (int, *domain.AppError) {
+	model := fromOrderToModel(order)
+	query := `insert into PurchaseOrder (UserID, ShippingAddressID, PaymentMethodID, 
+        Notes, OrderedAt) values (?,?,?,?,?)`
+
+	res, err := tx.Exec(query, model.UserID, model.ShippingAddressID,
+		model.PaymentMethodID, model.Notes.String, time.Now())
 	if err != nil {
 		return 0, rollbackAndError(tx, err)
 	}
+	orderID, _ := res.LastInsertId()
 
 	return int(orderID), nil
 }
 
-func updateOrderAmount(tx *sqlx.Tx, orderID int) error {
+func updateOrderAmount(tx *sqlx.Tx, orderID int) *domain.AppError {
 	var total float64
 
 	query := `select sum(pp.Quantity * p.Price) from ProductInOrder pp inner
 				join Product p on pp.ProductID = p.ID where OrderID=?`
 	err := tx.Get(&total, query, orderID)
 	if err != nil {
-		return err
+		return rollbackAndError(tx, err)
 	}
 
 	query = "update PurchaseOrder set TotalAmount=? where ID=?"
 	_, err = tx.Exec(query, total, orderID)
 	if err != nil {
-		return err
+		return rollbackAndError(tx, err)
 	}
 
 	err = tx.Commit()
 	if err != nil {
-		return err
+		return rollbackAndError(tx, err)
 	}
 
 	return nil
+}
+
+func rollbackAndError(tx *sqlx.Tx, err error) *domain.AppError {
+	_ = tx.Rollback()
+	logrus.WithField(misc.RequestIDKey, request.ID()).Error(err)
+
+	return domain.NewAppError(err, domain.RepositoryError)
 }
 
 func NewOrderRepository(db *sqlx.DB) *Repository {
